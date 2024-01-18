7dc93698ad75e6ab65c18395fb0d8d32f53503da
aimerzarashi <git.aimerzarashi.com>
2024-01-18 22:18:19 +0900

    added test directory

---
+++ test/root.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+describe("GET", () => {
+  it("正常系", async () => {
+    const response = await fetch(`http://localhost:3000/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    assert.strictEqual(
+      response.status,
+      200,
+      "ステータスコードが期待通りではありません");
+    assert.deepStrictEqual(
+      JSON.parse(await response.text()),
+      { message: "Hello, World!" },
+      "メッセージが期待通りではありません");
+  });
+});
