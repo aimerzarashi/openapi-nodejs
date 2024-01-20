7d50fa0ff536576a4d6354aa7a17ce57dd2c6826
aimerzarashi <git.aimerzarashi.com>
2024-01-20 16:25:09 +0900

    Added StockItem model to schema.prisma

---
+++ app/prisma/migrations/20240120045549_/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "StockItem" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "StockItem_pkey" PRIMARY KEY ("id")
+);
